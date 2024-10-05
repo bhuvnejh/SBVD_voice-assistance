6f374beeae5dd868ded52bba8dc9bcaca11ceac4
Bhuvnesh Chhajer <bhuvneshchhajer98@gmail.com>
2024-10-05 14:29:40 +0530

    hide

--- engine/db.py
+++ engine/db.py
@@ -20,7 +20,7 @@ cursor.execute(query)
 query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
 cursor.execute(query)
 
-# query = "INSERT INTO web_command VALUES (null,'college','https://ums.paruluniversity.ac.in/Login.aspx')"
+#
 # cursor.execute(query)
 # conn.commit()
 
